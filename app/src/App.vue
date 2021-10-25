242dc5ff369e5e5818010037500aecc9f0569406
Mathias Hörtnagl <m.hoertnagl@web-crossing.com>
2021-10-25 20:48:58 +0200

    lot of features

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
-const source = `
+  const source = `
 ---
 title: Test
 date: 2021-10-24 12:41
 tags: test
 ---
-# Test
+# Heading 1
+
+> Quote
+
+Inline KaTeX \`katex \\sum^{n}_{i=0} i\`
+
+\`\`\`js
+function foo() { return 0; }
+\`\`\`
+
+\`\`\`mermaid
+stateDiagram-v2
+    [*] --> Still
+    Still --> [*]
+
+    Still --> Moving
+    Moving --> Still
+    Moving --> Crash
+    Crash --> [*]
+\`\`\`
+
+\`\`\`katex
+\\sum^{n}_{i=0} i
+\`\`\`
 `
 </script>
 
@@ -14,10 +37,10 @@ tags: test
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
 </style>
--- app/src/components/ArticleRenderer.vue
+++ app/src/components/ArticleRenderer.vue
@@ -1,36 +1,73 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import * as matter from 'yaml-front-matter'
+  import { ref } from 'vue'
+  import * as matter from 'yaml-front-matter'
+  import marked, { MarkedExtension } from 'marked'
+  import hljs from 'highlight.js'
+  import 'highlight.js/styles/default.css'
+  import mermaid from 'mermaid'
+  import katex from 'katex'
+  import 'katex/dist/katex.css'
 
-const props = defineProps<{ source: string }>()
+  const props = defineProps<{ source: string }>()
 
-const contents = matter.loadFront(props.source.trim())
-console.log(contents);
+  mermaid.initialize({})
 
+  // https://marked.js.org/using_pro#extensions
+
+  const mermaidExtension: MarkedExtension = {
+    renderer: {
+      code(code, language) {
+        if (language === 'mermaid') {
+          return `<div class="mermaid">${code}</div>`
+        }
+        // Use default code renderer.
+        return false
+      },
+    },
+  }
+
+  const latexExtension: MarkedExtension = {
+    renderer: {
+      code(code, language) {
+        if (language === 'katex') {
+          return katex.renderToString(code, {
+            displayMode: true,
+          })
+        }
+        // Use default code renderer.
+        return false
+      },
+      codespan(code) {
+        if (code.startsWith('katex')) {
+          return katex.renderToString(code.substring(5))
+        }
+        return false
+      },
+    },
+  }
+
+  marked.setOptions({
+    highlight: (code, lang) => {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+      return hljs.highlight(code, { language }).value
+    },
+    langPrefix: 'hljs language-',
+  })
+
+  marked.use(mermaidExtension)
+  marked.use(latexExtension)
+
+  const cleanSource = props.source.trim()
+  const meta = matter.loadFront(cleanSource)
+  const contents = marked(meta.__content)
 </script>
 
 <template>
   <article>
-    <h1>{{ contents.title }}</h1>
-    <h5>{{ contents.date }}</h5>
-    {{ contents.__content }}
+    <h1>{{ meta.title }}</h1>
+    <h5>{{ meta.date }}</h5>
+    <section v-html="contents"></section>
   </article>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
+<style scoped></style>

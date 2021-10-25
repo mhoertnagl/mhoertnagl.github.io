6fbbee50f27a592e40d328ccb87434938627ddf7
Mathias Hörtnagl <m.hoertnagl@web-crossing.com>
2021-10-25 21:19:52 +0200

    new test

--- app/src/components/ArticleRenderer.vue
+++ app/src/components/ArticleRenderer.vue
@@ -1,50 +1,50 @@
 <script setup lang="ts">
   import { ref } from 'vue'
-  import * as matter from 'yaml-front-matter'
+  import matter from '../renderer/matter'
   import marked, { MarkedExtension } from 'marked'
   import hljs from 'highlight.js'
   import 'highlight.js/styles/default.css'
-  // import mermaid from 'mermaid'
-  // import katex from 'katex'
-  // import 'katex/dist/katex.css'
+  import mermaid from 'mermaid'
+  import katex from 'katex'
+  import 'katex/dist/katex.css'
 
   const props = defineProps<{ source: string }>()
 
-  // mermaid.initialize({})
+  mermaid.initialize({})
 
   // https://marked.js.org/using_pro#extensions
 
-  // const mermaidExtension: MarkedExtension = {
-  //   renderer: {
-  //     code(code, language) {
-  //       if (language === 'mermaid') {
-  //         return `<div class="mermaid">${code}</div>`
-  //       }
-  //       // Use default code renderer.
-  //       return false
-  //     },
-  //   },
-  // }
+  const mermaidExtension: MarkedExtension = {
+    renderer: {
+      code(code, language) {
+        if (language === 'mermaid') {
+          return `<div class="mermaid">${code}</div>`
+        }
+        // Use default code renderer.
+        return false
+      },
+    },
+  }
 
-  // const latexExtension: MarkedExtension = {
-  //   renderer: {
-  //     code(code, language) {
-  //       if (language === 'katex') {
-  //         return katex.renderToString(code, {
-  //           displayMode: true,
-  //         })
-  //       }
-  //       // Use default code renderer.
-  //       return false
-  //     },
-  //     codespan(code) {
-  //       if (code.startsWith('katex')) {
-  //         return katex.renderToString(code.substring(5))
-  //       }
-  //       return false
-  //     },
-  //   },
-  // }
+  const latexExtension: MarkedExtension = {
+    renderer: {
+      code(code, language) {
+        if (language === 'katex') {
+          return katex.renderToString(code, {
+            displayMode: true,
+          })
+        }
+        // Use default code renderer.
+        return false
+      },
+      codespan(code) {
+        if (code.startsWith('katex')) {
+          return katex.renderToString(code.substring(5))
+        }
+        return false
+      },
+    },
+  }
 
   marked.setOptions({
     highlight: (code, lang) => {
@@ -54,18 +54,18 @@
     langPrefix: 'hljs language-',
   })
 
-  // marked.use(mermaidExtension)
-  // marked.use(latexExtension)
+  marked.use(mermaidExtension)
+  marked.use(latexExtension)
 
   const cleanSource = props.source.trim()
-  const meta = matter.loadFront(cleanSource)
-  const contents = marked(meta.__content)
+  const document = matter(cleanSource)
+  const contents = marked(document.contents)
 </script>
 
 <template>
   <article>
-    <h1>{{ meta.title }}</h1>
-    <h5>{{ meta.date }}</h5>
+    <h1>{{ document.meta.title }}</h1>
+    <h5>{{ document.meta.date }}</h5>
     <section v-html="contents"></section>
   </article>
 </template>
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import importComponents from './autoreg'
 
 const app = createApp(App)

dbbb6d0ac6044ec57f310c7bb853479f46926352
Mathias Hörtnagl <m.hoertnagl@web-crossing.com>
2021-10-27 12:36:03 +0200

    rendering complete

--- gen/lib/commands/generate.js
+++ gen/lib/commands/generate.js
@@ -3,43 +3,78 @@ import glob from "fast-glob";
 import { join, parse } from "path";
 import handlebars from "handlebars";
 import renderer from "../renderer/renderer";
+class TemplatesMap extends Map {
+}
+class ArticleInfo {
+    path;
+    meta;
+}
 export async function generate() {
     const cwd = process.cwd();
     const srcDir = join(cwd, "src");
     const outDir = join(cwd, "docs");
+    await fs.ensureDir(outDir);
     const layouts = await loadLayouts(srcDir);
-    const srcBase = join(srcDir, "pages");
-    const outBase = join(outDir, "pages");
-    const pages = await glob("**/*.md", {
-        cwd: srcBase,
-        onlyFiles: true,
-    });
-    for (const page of pages) {
-        const srcPath = join(srcBase, page);
-        const contents = await fs.readFile(srcPath, "utf8");
-        const article = renderer.render(contents);
-        const layout = layouts.get(article.meta.layout);
-        if (layout) {
-            const outDirPath = join(outBase, parse(page).dir);
-            const outPath = join(outDirPath, `${parse(page).name}.html`);
-            const result = layout({ article });
-            await fs.ensureDir(outDirPath);
-            await fs.writeFile(outPath, result);
-        }
-    }
+    const articles = await createPages(srcDir, outDir, layouts);
+    await createStartPage(srcDir, outDir, articles);
 }
 async function loadLayouts(srcDir) {
-    const layouts = new Map();
+    const layouts = new TemplatesMap();
     const base = join(srcDir, "layouts");
-    const files = await glob("**/*.handlebars", {
-        cwd: base,
-        onlyFiles: true,
-    });
+    const files = await findFiles(base, "**/*.handlebars");
     for (const file of files) {
         const path = join(base, file);
-        const contents = await fs.readFile(path, "utf8");
-        const template = handlebars.compile(contents);
+        const template = await loadTemplate(path);
         layouts.set(parse(file).name, template);
     }
     return layouts;
+}
+async function createPages(srcDir, outDir, layouts) {
+    const articleInfos = [];
+    const srcBase = join(srcDir, "pages");
+    const outBase = join(outDir, "pages");
+    const pages = await findFiles(srcBase, "**/*.md");
+    for (const page of pages) {
+        const info = await createPage(srcBase, outBase, layouts, page);
+        if (info) {
+            articleInfos.push(info);
+        }
+    }
+    return articleInfos;
+}
+async function createPage(srcBase, outBase, layouts, page) {
+    const srcPath = join(srcBase, page);
+    const contents = await fs.readFile(srcPath, "utf8");
+    const article = renderer.render(contents);
+    const layout = layouts.get(article.meta.layout);
+    if (layout) {
+        const pageFile = parse(page);
+        const outFileName = `${pageFile.name}.html`;
+        const outDirPath = join(outBase, pageFile.dir);
+        const outPath = join(outDirPath, outFileName);
+        const result = layout({ article });
+        await fs.ensureDir(outDirPath);
+        await fs.writeFile(outPath, result);
+        const info = new ArticleInfo();
+        info.path = join("pages", pageFile.dir, outFileName);
+        info.meta = article.meta;
+        return info;
+    }
+}
+async function createStartPage(srcDir, outDir, articles) {
+    const srcPath = join(srcDir, "index.handlebars");
+    const outPath = join(outDir, "index.html");
+    const template = await loadTemplate(srcPath);
+    const result = template({ articles });
+    await fs.writeFile(outPath, result);
+}
+function findFiles(base, pattern) {
+    return glob(pattern, {
+        cwd: base,
+        onlyFiles: true,
+    });
+}
+async function loadTemplate(path) {
+    const contents = await fs.readFile(path, "utf8");
+    return handlebars.compile(contents);
 }
--- gen/src/commands/generate.ts
+++ gen/src/commands/generate.ts
@@ -4,6 +4,13 @@ import { join, parse } from "path";
 import handlebars from "handlebars";
 import renderer from "../renderer/renderer";
 
+class TemplatesMap extends Map<string, HandlebarsTemplateDelegate<any>> {}
+
+class ArticleInfo {
+  path: string;
+  meta: any;
+}
+
 // const spinner = ora({
 //   text: "Simulating some slow async task. What a Devimal Planet...",
 //   spinner: "earth",
@@ -14,31 +21,13 @@ export async function generate() {
   const cwd = process.cwd();
   const srcDir = join(cwd, "src");
   const outDir = join(cwd, "docs");
+
+  await fs.ensureDir(outDir);
+
   const layouts = await loadLayouts(srcDir);
+  const articles = await createPages(srcDir, outDir, layouts);
 
-  const srcBase = join(srcDir, "pages");
-  const outBase = join(outDir, "pages");
-  const pages = await glob("**/*.md", {
-    cwd: srcBase,
-    onlyFiles: true,
-  });
-
-  for (const page of pages) {
-    const srcPath = join(srcBase, page);
-    const contents = await fs.readFile(srcPath, "utf8");
-    const article = renderer.render(contents);
-    const layout = layouts.get(article.meta.layout);
-    if (layout) {
-      const outDirPath = join(outBase, parse(page).dir);
-      const outPath = join(outDirPath, `${parse(page).name}.html`);
-      const result = layout({ article });
-      await fs.ensureDir(outDirPath);
-      await fs.writeFile(outPath, result);
-    }
-  }
-  // Read page contents
-  // Compile (front-matter + markdown)
-
+  await createStartPage(srcDir, outDir, articles);
   // Read index.handlebars
 
   // Create HTML files
@@ -47,19 +36,70 @@ export async function generate() {
 }
 
 async function loadLayouts(srcDir: string) {
-  const layouts = new Map<string, HandlebarsTemplateDelegate>();
+  const layouts = new TemplatesMap();
   const base = join(srcDir, "layouts");
-  const files = await glob("**/*.handlebars", {
-    cwd: base,
-    onlyFiles: true,
-  });
+  const files = await findFiles(base, "**/*.handlebars");
   for (const file of files) {
     const path = join(base, file);
-    const contents = await fs.readFile(path, "utf8");
-    const template = handlebars.compile(contents);
+    const template = await loadTemplate(path);
     layouts.set(parse(file).name, template);
   }
   return layouts;
+}
+
+async function createPages(
+  srcDir: string,
+  outDir: string,
+  layouts: TemplatesMap
+) {
+  const articleInfos: ArticleInfo[] = [];
+  const srcBase = join(srcDir, "pages");
+  const outBase = join(outDir, "pages");
+  const pages = await findFiles(srcBase, "**/*.md");
+  for (const page of pages) {
+    const info = await createPage(srcBase, outBase, layouts, page);
+    if (info) {
+      articleInfos.push(info);
+    }
+  }
+  return articleInfos;
+}
+
+async function createPage(
+  srcBase: string,
+  outBase: string,
+  layouts: TemplatesMap,
+  page: string
+) {
+  const srcPath = join(srcBase, page);
+  const contents = await fs.readFile(srcPath, "utf8");
+  const article = renderer.render(contents);
+  const layout = layouts.get(article.meta.layout);
+  if (layout) {
+    const pageFile = parse(page);
+    const outFileName = `${pageFile.name}.html`;
+    const outDirPath = join(outBase, pageFile.dir);
+    const outPath = join(outDirPath, outFileName);
+    const result = layout({ article });
+    await fs.ensureDir(outDirPath);
+    await fs.writeFile(outPath, result);
+    const info = new ArticleInfo();
+    info.path = join("pages", pageFile.dir, outFileName);
+    info.meta = article.meta;
+    return info;
+  }
+}
+
+async function createStartPage(
+  srcDir: string,
+  outDir: string,
+  articles: ArticleInfo[]
+) {
+  const srcPath = join(srcDir, "index.handlebars");
+  const outPath = join(outDir, "index.html");
+  const template = await loadTemplate(srcPath);
+  const result = template({ articles });
+  await fs.writeFile(outPath, result);
 }
 
 // async function copyAssets(srcDir: string, outDir: string) {
@@ -70,3 +110,15 @@ async function loadLayouts(srcDir: string) {
 //     overwrite: true,
 //   });
 // }
+
+function findFiles(base: string, pattern: string) {
+  return glob(pattern, {
+    cwd: base,
+    onlyFiles: true,
+  });
+}
+
+async function loadTemplate(path: string) {
+  const contents = await fs.readFile(path, "utf8");
+  return handlebars.compile(contents);
+}

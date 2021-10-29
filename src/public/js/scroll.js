c40d557091c03067d73de04671cbbc02849cc7bb
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2021-10-30 00:21:01 +0200

    fix scroll logic

--- src/public/js/scroll.js
+++ src/public/js/scroll.js
@@ -1,29 +1,40 @@
-let progress = document.getElementById('article-progess');
-let documentHeight = document.body.scrollHeight;
-let documentWidth = document.body.scrollWidth;
-let lastKnownScrollPosition = 0;
-let ticking = false;
+class ProgressBar {
 
-function updateProgress(scrollPos) {
-  const factor = Math.min(scrollPos / documentHeight, 1);
-  const width = factor * documentWidth + 'px';
-  progress.style.width = width;
+  constructor(id) {
+    this.progress = document.getElementById(id);
+    this.documentHeight = document.body.scrollHeight;
+    this.documentWidth = document.body.scrollWidth;
+    this.lastKnownScrollPosition = 0;
+    this.ticking = false;
+
+    document.addEventListener('scroll', () => this.onScroll());
+    window.addEventListener('resize', () => this.onResize());
+  }
+
+  onScroll() {
+    this.lastKnownScrollPosition = window.scrollY;
+
+    if (!this.ticking) {
+      window.requestAnimationFrame(() => {
+        this.updateProgress(this.lastKnownScrollPosition);
+        this.ticking = false;
+      });
+
+      this.ticking = true;
+    }
+  }
+
+  onResize() {
+    this.documentWidth = document.body.scrollWidth;
+    this.updateProgress(this.lastKnownScrollPosition);
+  }
+
+  updateProgress(scrollPos) {
+    const factor = Math.min(scrollPos / this.documentHeight, 1);
+    // Some weird failsafe. Maybe obsolete when there is a footer.
+    const width = ((factor > 0.97) ? 1 : factor) * this.documentWidth + 'px';
+    this.progress.style.width = width;
+  }
 }
 
-document.addEventListener('scroll', function(e) {
-  lastKnownScrollPosition = window.scrollY;
-
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      updateProgress(lastKnownScrollPosition);
-      ticking = false;
-    });
-
-    ticking = true;
-  }
-});
-
-window.addEventListener('resize', function () {
-  documentWidth = document.body.scrollWidth;
-  updateProgress(lastKnownScrollPosition);
-});
+new ProgressBar('article-progess');

ae9f28b022d6f98e93a13986041ad72d2561f851
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2021-10-31 01:17:48 +0200

    ...

--- src/public/js/scroll.js
+++ src/public/js/scroll.js
@@ -4,22 +4,18 @@ class ProgressBar {
     this.progress = document.getElementById(id);
     this.lastKnownScrollPosition = 0;
     this.ticking = false;
-
     document.addEventListener('scroll', () => this.onScroll());
     window.addEventListener('resize', () => this.onResize());
-
     this.getDocumentSize();
   }
 
   onScroll() {
     this.lastKnownScrollPosition = window.scrollY;
-
     if (!this.ticking) {
       window.requestAnimationFrame(() => {
         this.updateProgress(this.lastKnownScrollPosition);
         this.ticking = false;
       });
-
       this.ticking = true;
     }
   }
@@ -30,17 +26,19 @@ class ProgressBar {
   }
 
   getDocumentSize() {
-    this.documentHeight = document.body.scrollHeight;
+    this.documentHeight = document.body.scrollHeight - document.body.clientHeight;
     this.documentWidth = document.body.scrollWidth;
+    console.log(`Height: ${this.documentHeight}, Width: ${this.documentWidth}`);
   }
 
   updateProgress(scrollPos) {
     const factor = Math.min(scrollPos / this.documentHeight, 1);
-    // Some weird failsafe. Maybe obsolete when there is a footer.
-    const width = ((factor > 0.97) ? 1 : factor) * this.documentWidth + 'px';
+    const width = factor * this.documentWidth + 'px';
     this.progress.style.width = width;
+    console.log(scrollPos);
   }
 }
+
 document.addEventListener("DOMContentLoaded", () => {
   new ProgressBar('article-progess');
 });
